2a865cbd3db48ad94aeaecf4ee11fbbfe929f5ea
daweeed <david.forgeron@gmail.com>
2015-04-29 00:09:09 +0200

    removing bug

--- poppy.js
+++ poppy.js
@@ -2,5 +2,4 @@
 var poppycreatures = [
   {'lat': 48.896014, 'lon': 2.38746, 'desc': "<a href='http://www.cite-sciences.fr/fr/carrefour-numerique'>Carrefour Numerique</a> : 1 Poppy humanoide et 1 ergorobot"},
   {'lat': 44.808114, 'lon': -0.599977, 'desc': "<a href='https://flowers.inria.fr/'>INRIA - FLowers</a>"}
-44.808114,-0.599977
 ]
