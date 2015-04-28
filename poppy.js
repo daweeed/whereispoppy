eebb4a9ea74a8bbe0448eee672db890d1665408b
daweeed <david.forgeron@gmail.com>
2015-04-29 00:08:36 +0200

    Adding Inria

--- poppy.js
+++ poppy.js
@@ -1,4 +1,6 @@
 // add a line into this Array
 var poppycreatures = [
-  {'lat': 48.896014, 'lon': 2.38746, 'desc': "<a href='http://www.cite-sciences.fr/fr/carrefour-numerique'>Carrefour Numerique</a>"}
+  {'lat': 48.896014, 'lon': 2.38746, 'desc': "<a href='http://www.cite-sciences.fr/fr/carrefour-numerique'>Carrefour Numerique</a> : 1 Poppy humanoide et 1 ergorobot"},
+  {'lat': 44.808114, 'lon': -0.599977, 'desc': "<a href='https://flowers.inria.fr/'>INRIA - FLowers</a>"}
+44.808114,-0.599977
 ]
