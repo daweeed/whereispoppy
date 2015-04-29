9c7b78c4c57d4f60e04d3a61e2e985becaf39884
daweeed <david.forgeron@gmail.com>
2015-04-29 14:21:29 +0200

    Adding  "," at the end of lines to poppy.js

--- poppy.js
+++ poppy.js
@@ -1,9 +1,9 @@
 // add a line into this Array
 var poppycreatures = [
   {'lat': 48.896014, 'lon': 2.38746, 'desc': "<a href='http://www.cite-sciences.fr/fr/carrefour-numerique'>Carrefour Numerique</a> : 1 Poppy humanoide et 1 ergorobot"},
-  {'lat': 44.808114, 'lon': -0.599977, 'desc': "<a href='https://flowers.inria.fr/'>INRIA - FLowers</a>: 2 Poppy Humanoid, 1 Poppy Humanoid Beta, 1 Poppy Ergo, 1 Poppy ErgoJr"}
-  {'lat': 44.802722, 'lon': -0.602083, 'desc': "<a href='http://www.ensam.eu/'>Arts et Métiers ParisTech</a>: 1 Poppy Humanoid Beta"}
-  {'lat': 44.832091, 'lon': -0.579934, 'desc': "<a href='http://www.lyceesaintefamille.com/'>Lycée Sainte-Famille Saintonge</a>: 2 buste de Poppy Humanoid Beta"}
-  {'lat': 48.833599, 'lon': 2.358404, 'desc': "<a href='http://www.ensam.eu/'>Arts et Métiers ParisTech</a>: 1 Poppy Humanoid Beta"}
+  {'lat': 44.808114, 'lon': -0.599977, 'desc': "<a href='https://flowers.inria.fr/'>INRIA - FLowers</a>: 2 Poppy Humanoid, 1 Poppy Humanoid Beta, 1 Poppy Ergo, 1 Poppy ErgoJr"},
+  {'lat': 44.802722, 'lon': -0.602083, 'desc': "<a href='http://www.ensam.eu/'>Arts et Métiers ParisTech</a>: 1 Poppy Humanoid Beta"},
+  {'lat': 44.832091, 'lon': -0.579934, 'desc': "<a href='http://www.lyceesaintefamille.com/'>Lycée Sainte-Famille Saintonge</a>: 2 buste de Poppy Humanoid Beta"},
+  {'lat': 48.833599, 'lon': 2.358404, 'desc': "<a href='http://www.ensam.eu/'>Arts et Métiers ParisTech</a>: 1 Poppy Humanoid Beta"},
   {'lat': 48.848849, 'lon': 2.345485, 'desc': "<a href='http://www.college-de-france.fr/'>Collège de France</a>: 1 Poppy Humanoid Beta"}
 ]
