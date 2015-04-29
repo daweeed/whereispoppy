309987d3854838f77c0edc08aa5e7200c514af6b
Manon Cortial <mc@generationrobots.com>
2015-04-29 10:03:20 +0200

    Update poppy.js

--- poppy.js
+++ poppy.js
@@ -2,4 +2,5 @@
 var poppycreatures = [
   {'lat': 48.896014, 'lon': 2.38746, 'desc': "<a href='http://www.cite-sciences.fr/fr/carrefour-numerique'>Carrefour Numerique</a> : 1 Poppy humanoide et 1 ergorobot"},
   {'lat': 44.808114, 'lon': -0.599977, 'desc': "<a href='https://flowers.inria.fr/'>INRIA - FLowers</a>"}
+  {'lat': 44.875772, 'lon': -0.577901, 'desc': "<a href='http://www.generationrobots.com/fr/279-poppy-plateforme-robotique-opensource'>Génération Robots / Humarobotics</a>"}
 ]
